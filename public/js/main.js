5d8ff7c226f09aec6e6882e9fe3dde56141c057e
Mateus de Medeiros Cordeiro <mateusmcordeiro@hotmail.com>
2019-01-10 14:46:23 -0200

    correção menu

--- public/js/main.js
+++ public/js/main.js
@@ -4,19 +4,15 @@ $(document).ready(function(){
         if($(".navbar-burger").hasClass('is-active')){
             $(".navbar-burger").removeClass('is-active');
             $(".navbar-menu").css('display','none');
-            $(".navbar-menu").removeClass('navbar-rits');
         }else{
             $(".navbar-burger").addClass('is-active');
             $(".navbar-menu").css('display','block');
-            $(".navbar-menu").addClass('navbar-rits');
         }
     });
     $(window).resize(function () {
         if (jQuery(window).width() > 1087) {
             $(".navbar-burger").removeClass('is-active');
-            $(".navbar-menu").removeClass('navbar-rits');
         }else{
-            $(".navbar-menu").css('display','none');
         }
     });
 });
